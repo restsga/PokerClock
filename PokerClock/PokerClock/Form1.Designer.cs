66bd8c72095307884371479979e2f85d78ad1664
restsga <restsga@gmail.com>
2020-06-13 10:25:05 +0900

    v0.1.0

---
+++ PokerClock/PokerClock/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace PokerClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Path";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 128F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(760, 168);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "99:99";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 526);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "-Level";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(697, 526);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "+Level";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(137, 526);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "+10min";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(218, 526);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "+1min";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(299, 526);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "+10sec";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(407, 526);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "-10sec";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(760, 171);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "10000/20000\r\n20000";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(408, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(364, 126);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Next\r\n20000/40000\r\n40000";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "log";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(488, 526);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "-1min";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(570, 526);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "-10min";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ PokerClock/PokerClock/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PokerClock
+{
+    public partial class Form1 : Form
+    {
+        //ストラクチャーデータのサイズとインデックス
+        const int DATA_SIZE = 4;
+        const int SB = 0, BB = 1, ANTE = 2, TIME = 3;
+        //タイマーの処理間隔(Hz)
+        const int TIMER_HZ = 10;
+
+        //ストラクチャーデータ(一次元)
+        List<int> blind_structure = new List<int>();
+
+        //クロックの動作フラグ
+        bool clock_enable = false;
+        //開始時間
+        DateTime startTime=DateTime.Now;
+        //経過時間
+        int time = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //ファイル読み込み
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //ダイアログ表示
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //決定ボタンを押した
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //ファイルを開く
+                    Stream stream = openFileDialog.OpenFile();
+
+                    //読み込む
+                    using(StreamReader reader=new StreamReader(stream))
+                    {
+                        try
+                        {
+                            //ストラクチャーデータを削除
+                            blind_structure.Clear();
+                            //1行読み込み
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                //直接数値に変換して保存
+                                blind_structure.AddRange(line.Split(',').Select(s => int.Parse(s)).ToList());
+                            }
+                            //再描画
+                            RepaintText();
+                            //完了通知
+                            label4.Text = "Load Complete";
+                        }
+                        catch(FormatException exception)
+                        {
+                            //例外発生時はストラクチャーデータはnullとする
+                            blind_structure.Clear();
+                            //エラー通知
+                            label4.Text = "Error";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //クロック動作を反転
+            clock_enable = clock_enable ? false : true;
+            //開始時間のリセット
+            startTime = DateTime.Now;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+                //経過時間をリセット
+                time = 0;
+                //再描画
+                RepaintText();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+            //残り時間とブラインドレベルのインデックス
+            int limit, index;
+            (limit, index) = LimitTime();
+
+            //リスト外部への参照防止
+            if (index <= 0)
+            {
+                time = 0;
+            }
+            else
+            {
+                time -= limit + blind_structure[(index - 1) * DATA_SIZE + TIME]*60;
+            }
+
+            RepaintText();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+            //残り時間とブラインドレベルのインデックス
+            int limit, index;
+            (limit, index) = LimitTime();
+
+            //リスト外部への参照防止
+            if ((index+1)*DATA_SIZE >= blind_structure.Count)
+            {
+                time = int.MaxValue;
+            }
+            else
+            {
+                time +=limit;
+            }
+
+            RepaintText();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+
+                time -= 10 * 60;
+
+            RepaintText();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+
+                time -= 1 * 60;
+
+            RepaintText();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+
+                time -= 10;
+
+            RepaintText();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+
+                time += 10;
+
+            RepaintText();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+
+                time += 1*60;
+
+            RepaintText();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //クロック停止時のみ動作
+            if (clock_enable)
+            {
+                return;
+            }
+
+                time += 10*60;
+
+            RepaintText();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //クロック動作時
+            if (clock_enable)
+            {
+                if (DateTime.Now.AddSeconds((time+1)*(-1)) >= startTime)
+                {
+                    //時間を進める
+                    time++;
+                    //再描画
+                    RepaintText();
+                }
+            }
+        }
+
+        private void RepaintText()
+        {
+            //残り時間とブラインドレベルのインデックス
+            int limit, index;
+            (limit, index) = LimitTime();
+
+            //時間表示
+            string nowTime = "0:00";
+            if (index * DATA_SIZE < blind_structure.Count)
+            {
+                nowTime= ((limit / 60) + ":" + string.Format("{0:D2}",(limit % 60)));
+            }
+            //現在のブラインド表示
+            string nowBlind ="End Tournament";
+            if (index * DATA_SIZE < blind_structure.Count)
+            {
+                nowBlind=
+                    blind_structure[index * DATA_SIZE + SB] + "/" + blind_structure[index * DATA_SIZE + BB] +
+                    Environment.NewLine + blind_structure[index * DATA_SIZE + ANTE];
+            }
+            //次のブラインド表示
+            string nextBlind = "Final Blind";
+            if ((index + 1) * DATA_SIZE < blind_structure.Count)
+            {
+                nextBlind=
+                    "Next" + Environment.NewLine +
+                    blind_structure[(index + 1) * DATA_SIZE + SB] + "/" + blind_structure[(index + 1) * DATA_SIZE + BB]+
+                    Environment.NewLine+blind_structure[(index+1)*DATA_SIZE+ANTE];
+            }
+
+            //表示を更新する
+            label1.Text = nowTime;
+            label2.Text = nowBlind;
+            label3.Text = nextBlind;
+        }
+
+        private (int,int) LimitTime()
+        {
+            //値をいじるのでコピー
+            int limit = time;
+            //注目しているレベルの時間
+            int levelTime;
+            //注目しているレベルのインデックス
+            int index = 0;
+            //現在のレベルに到達するまで探索
+            while (limit >= (levelTime = blind_structure[index * DATA_SIZE + TIME] * 60))
+            {
+                //レベルでの所要時間を消費
+                limit -= levelTime;
+                //インデックスをずらす
+                index++;
+                //リスト外部の参照防止
+                if(index * DATA_SIZE >= blind_structure.Count)
+                {
+                    return (0, index);
+                }
+            }
+
+            limit = blind_structure[index * DATA_SIZE + TIME] * 60 - limit;
+
+            return (limit,index);
+        }
+    }
+}
